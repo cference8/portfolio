1d84931700240e2962adf28871796be5d7bbf7bd
Christopher Ference <christopherference8@gmail.com>
2022-04-01 00:31:20 -0500

    update nav bar

--- script.js
+++ script.js
@@ -10,7 +10,25 @@ function printDiv(divName) {
     document.body.innerHTML = originalContents;
 }
 
-// Nav Bar
+// mobile nav Bar
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
 
 // Flappy Bird
 var myGamePiece;
