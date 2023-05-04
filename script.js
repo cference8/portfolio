9fc6561d499006d3698456da8551fdcf3c7779e7
Christopher Ference <christopherference8@gmail.com>
2023-05-04 01:06:35 -0500

    update

--- script.js
+++ script.js
@@ -1,61 +1,56 @@
-$("#my-form").on("submit", function() {
-    // Form script
-	window.addEventListener('DOMContentLoaded', function () {
-		// get the form elements defined in your form HTML above
+// FORMSPREE.IO -- start
+$("#my-form").on("submit", function () {
+  // Form script
+  window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
 
-		var form = document.getElementById('my-form');
-		// var button = document.getElementById("my-form-button");
-		var status = document.getElementById('status');
+    var form = document.getElementById("my-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
 
-		// Success and Error functions for after the form is submitted
+    // Success and Error functions for after the form is submitted
 
-		function success() {
-			form.reset();
-			status.classList.add('success');
-			status.innerHTML = "Thanks for your submission!";
-		}
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "Thanks for your submission!";
+    }
 
-		function error() {
-			status.classList.add('error');
-			status.innerHTML = "Oops! There was a problem submitting your form";
-			alert("Try again");
-		}
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem submitting your form";
+      alert("Try again");
+    }
 
-		// handle the form submission event
+    // handle the form submission event
 
-		form.addEventListener('submit', function (ev) {
-			ev.preventDefault();
-			var data = new FormData(form);
-			ajax(form.method, form.action, data, success, error);
-		});
-	});
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
 
-  // helper function for sending an AJAX request
+    // helper function for sending an AJAX request
 
-  function ajax(method, url, data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.onreadystatechange = function () {
+    function ajax(method, url, data, success, error) {
+      var xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.setRequestHeader("Accept", "application/json");
+      xhr.onreadystatechange = function () {
         if (xhr.readyState !== XMLHttpRequest.DONE) return;
         if (xhr.status === 200) {
-            success(xhr.response, xhr.responseType);
+          success(xhr.response, xhr.responseType);
         } else {
-            error(xhr.status, xhr.response, xhr.responseType);
+          error(xhr.status, xhr.response, xhr.responseType);
         }
-    };
-    xhr.send(data);
-  }
+      };
+      xhr.send(data);
+    }
+  });
 });
+// FORMSPREE.IO -- end
 
-
-function printDiv(divName) {
-  var printContents = document.getElementById(divName).innerHTML,
-    originalContents = document.body.innerHTML;
-  (document.body.innerHTML = printContents),
-    window.print(),
-    (document.body.innerHTML = originalContents);
-}
+// FLAPPY BIRD GAME -- start
 var myGamePiece,
   myObstacles = [],
   myScore;
@@ -182,4 +177,15 @@ function everyinterval(n) {
 }
 function accelerate(n) {
   myGamePiece.gravity = n;
+}
+// FLAPPY BIRD GAME -- end
+
+
+// Print any div
+function printDiv(divName) {
+  var printContents = document.getElementById(divName).innerHTML,
+    originalContents = document.body.innerHTML;
+  (document.body.innerHTML = printContents),
+    window.print(),
+    (document.body.innerHTML = originalContents);
 }
